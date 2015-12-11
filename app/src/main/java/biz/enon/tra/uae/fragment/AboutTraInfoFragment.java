e57a08db44d259644cf2dd361542996e63407e66
mobimaks <mobimaks13@gmail.com>
2015-12-11 17:33:47 +0200

    AboutTra: make image gray scale into B&W mode

--- app/src/main/java/biz/enon/tra/uae/fragment/AboutTraInfoFragment.java
+++ app/src/main/java/biz/enon/tra/uae/fragment/AboutTraInfoFragment.java
@@ -1,16 +1,20 @@
 package biz.enon.tra.uae.fragment;
 
 import android.annotation.SuppressLint;
+import android.os.Bundle;
 import android.webkit.WebView;
+import android.widget.ImageView;
 
 import biz.enon.tra.uae.R;
 import biz.enon.tra.uae.fragment.base.BaseFragment;
+import biz.enon.tra.uae.util.ImageUtils;
 
 /**
  * Created by mobimaks on 16.09.2015.
  */
 public class AboutTraInfoFragment extends BaseFragment {
 
+    private ImageView tvHeaderImage;
     private WebView wvWebView;
 
     public static AboutTraInfoFragment newInstance() {
@@ -21,9 +25,16 @@ public class AboutTraInfoFragment extends BaseFragment {
     @Override
     protected void initViews() {
         super.initViews();
+        tvHeaderImage = findView(R.id.tvHeaderImage_FAT);
         wvWebView = findView(R.id.wvText_FAT);
         wvWebView.getSettings().setJavaScriptEnabled(true);
         wvWebView.loadDataWithBaseURL(null, getString(R.string.fragment_about_tra_html), "text/html", "utf-8", null);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        tvHeaderImage.setImageDrawable(ImageUtils.getFilteredDrawable(getActivity(), R.drawable.logo));
     }
 
     @Override
