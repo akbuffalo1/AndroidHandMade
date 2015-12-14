9bd9198856f51781f8074c06fefb2d94f3369aaa
ak-buffalo <ak-buffalo@mail.ru>
2015-12-14 15:56:50 +0200

    TransactionsDetailsFragment - change request - DONE.

--- app/src/main/java/biz/enon/tra/uae/fragment/TransactionDetailsFragment.java
+++ app/src/main/java/biz/enon/tra/uae/fragment/TransactionDetailsFragment.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import biz.enon.tra.uae.R;
 import biz.enon.tra.uae.customviews.HexagonView;
@@ -27,26 +28,25 @@ import static biz.enon.tra.uae.global.C.TRANSACTION_STATUS_ICON_INDEX;
 public class TransactionDetailsFragment extends BaseFragment implements OnClickListener {
 
     private static final String KEY_TRANSACTION = "TRANSACTION";
-    private static final String KEY_ICON_COLOR = "ICON_COLOR";
+    private static final String KEY_ICON_RES_AND_COLOR = "ICON_COLOR";
 
     public static TransactionDetailsFragment newInstance(int[] _icon_color, GetTransactionResponseModel _model) {
         final TransactionDetailsFragment fragment = new TransactionDetailsFragment();
         final Bundle args = new Bundle();
         args.putParcelable(KEY_TRANSACTION, _model);
-        args.putIntArray(KEY_ICON_COLOR, _icon_color);
+        args.putIntArray(KEY_ICON_RES_AND_COLOR, _icon_color);
         fragment.setArguments(args);
         return fragment;
     }
 
     private HexagonView hexagonView;
-    private TextView tvTile;
-    private TextView tvDescription;
-    private TextView tvComplainInformation;
+    private TextView tvTile, tvDescription, tvComplainInformation, tvDate, tvStatus;
     private Button btnEdit;
 
     private OnServiceSelectListener mOnServiceSelectListener;
 
     private GetTransactionResponseModel mTransaction;
+    private Service service;
     private int[] mIconColor;
 
     @Override
@@ -63,7 +63,7 @@ public class TransactionDetailsFragment extends BaseFragment implements OnClickL
 
         final Bundle args = getArguments();
         mTransaction = args.getParcelable(KEY_TRANSACTION);
-        mIconColor = args.getIntArray(KEY_ICON_COLOR);
+        mIconColor = args.getIntArray(KEY_ICON_RES_AND_COLOR);
     }
 
     @Override
@@ -96,11 +96,18 @@ public class TransactionDetailsFragment extends BaseFragment implements OnClickL
         tvTile = findView(R.id.hvTitle_FTD);
         tvTile.setText(mTransaction.title);
 
-        tvDescription = findView(R.id.hvDescr_FTD);
+        tvDescription = findView(R.id.tvDescription_FTD);
         tvDescription.setText(mTransaction.description);
 
-        tvComplainInformation = findView(R.id.tvComplainInformation_FTD);
+        tvComplainInformation = findView(R.id.tvDescription_FTD);
         tvComplainInformation.setText(mTransaction.description);
+
+        tvDate = findView(R.id.tvDate_FTD);
+        tvDate.setText(mTransaction.modifiedDatetime);
+
+        tvStatus = findView(R.id.tvStatusValue_FTD);
+        tvStatus.setText(mTransaction.statusCode);
+        tvStatus.setTextColor(getResources().getColor(mIconColor[1]));
 
         btnEdit = findView(R.id.btnEdit_FTD);
         btnEdit.setVisibility(/*WAITING_FOR_DETAILS.equals(mTransaction.statusCode) ? View.VISIBLE :*/ View.GONE);
@@ -115,13 +122,14 @@ public class TransactionDetailsFragment extends BaseFragment implements OnClickL
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        getToolbarTitleManager().setTitle(mTransaction.title);
+        service = Service.getServiceTypeByString(mTransaction.type);
+        getToolbarTitleManager().setTitle(service.getTransactionName());
     }
 
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.btnEdit_FTD && mOnServiceSelectListener != null) {
-            mOnServiceSelectListener.onServiceSelect(Service.getServiceTypeByString(mTransaction.type), mTransaction);
+            mOnServiceSelectListener.onServiceSelect(service, mTransaction);
         }
     }
 
